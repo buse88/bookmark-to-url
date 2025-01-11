484c23aac9f86b67335ee102e65814ba6292c81c
buse88 <79825350+buse88@users.noreply.github.com>
2025-01-11 19:02:11 +0800

    Add files via upload

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+// 监听扩展的安装事件
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('扩展已安装');
+  console.log("扩展已安装！");
+});
+
+// 监听书签的创建事件
+chrome.bookmarks.onCreated.addListener((id, bookmark) => {
+  console.log("新书签创建：", bookmark);
 });
